19c12ddb7916768c9e4827a2c4ab34d5a2830213
Riad374-code <riad.shiraliyev.018@gmail.com>
2026-04-10 17:23:21 +0400

    Saving feature fully added

--- src/trainable_model_integ/gpu_detailing.rs
+++ src/trainable_model_integ/gpu_detailing.rs
@@ -24,25 +24,6 @@ pub enum Errors {
     InvalidData,
 }
 
-fn get_perandtemp()-> Result<Vec<GpuDetailingVisitor>,NvmlError>{
-    let nvml=Nvml::init()?;
-    let device =nvml.device_by_index(0)?;
-    let sensors=enum_wrappers::device::TemperatureSensor::Gpu;
-    let mut detail:Vec<GpuDetailingVisitor>=Vec::new();
-    for _ in 1..1000{
-        let temprature=device.temperature(sensors)?;
-        let power_usage=device.power_usage()?;
-        let usage_perc=device.utilization_rates()?;
-        detail.push(GpuDetailingVisitor{
-            temprature,
-            power_usage,
-            usage_percent:usage_perc.gpu,
-        });
-        std::thread::sleep(std::time::Duration::from_millis(1000));
-    }
-    Ok(detail)
-}
-
 fn create_file()->Result<File,std::io::Error>{
     let file=std::fs::File::create(Path::new(r"C:\Users\user\OneDrive - Baku Higher Oil School\Desktop\MiniRustProjects\PerformanceController\PerformanceController\src\trainable_model_integ\gpu_info.csv")).unwrap();
     Ok(file)
@@ -57,11 +38,35 @@ pub fn file_saver(writable: GpuDetailingVisitor)-> Result<(),Errors>{
     };
     let mut write=csv::Writer::from_writer(file);
 
-    write.serialize(writable);
+    if let Err(e) =write.serialize(writable){
+        warn!("Could not write the file");
+        return Err(Errors::CsvIssue(e));
+    }
 
-    write.flush()?;
-
-
+    if let Err(e)=write.flush(){
+        warn!("Data is not in buffer");
+        return Err(Errors::FileIssue(e));
+    }
     Ok(())
+}
 
+fn get_perandtemp()-> Result<(),NvmlError>{
+    let nvml=Nvml::init()?;
+    let device =nvml.device_by_index(0)?;
+    let sensors=enum_wrappers::device::TemperatureSensor::Gpu;
+    for _ in 1..1000{
+        let temprature=device.temperature(sensors)?;
+        let power_usage=device.power_usage()?;
+        let usage_perc=device.utilization_rates()?;
+        match file_saver(GpuDetailingVisitor{
+            temprature,
+            power_usage,
+            usage_percent:usage_perc.gpu,
+        }){
+            Ok(())=>{},
+            Err(e)=>{break}
+        }
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+    }
+    Ok(())
 }
